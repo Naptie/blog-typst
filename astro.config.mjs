a6b7dd991b682415c781710ae8d0221cad480e7b
Naptie <naptie@qq.com>
2025-05-23 21:00:07 +0800

    fix: env variable handling

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,16 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import { typst } from "astro-typst";
-import { URL_BASE } from "./config.json";
+import { URL_BASE as DEFAULT_URL_BASE } from "./config.json";
+import { loadEnv } from "vite";
+
+const { URL_BASE } = loadEnv(process.env.NODE_ENV ?? "", process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
   // Deploys to GitHub Pages
   site: "https://naptie.github.io",
-  base: process.env.URL_BASE ?? URL_BASE,
+  base: URL_BASE ?? DEFAULT_URL_BASE,
 
   integrations: [
     sitemap(),
